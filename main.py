26a9c8c662bfb94c2dadc271bfac3add455f5122
LastWeekNextDay <doesn@mail.ru>
2023-09-21 22:26:04 +0300

    Oh

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ wb_test = load_workbook(filename='Input.xlsx')
 ws_test = wb_test.active
 colA_test = ws_test['A']
 X_test = np.array([])
-a = deque()
+a.clear()
 for X in colA_test:
     T = X.value.split(' ')
     if len(T) > 2:
@@ -37,11 +37,33 @@ for X in colA_test:
     E = list(R)
     a.append(E)
 X_test = np.array(a)
-print(X_test)
 
 #Prediction
 knn = KNN(k=3)
 knn.fit(X_set, Y_set)
 Y_pred = knn.predict(X_test)
 
-print(Y_pred)
+#Write to file
+X_set = np.append(X_set, X_test, axis=0)
+Y_set = np.append(Y_set, Y_pred, axis=0)
+
+#seen = set()
+#X_set = [item for item in X_set if not(tuple(item) in seen or seen.add(tuple(item)))]
+#print(X_set)
+
+
+
+resultX = []
+resultY = []
+iter = 0
+for i in X_set:
+    in_list = any(np.array_equal(i, arr) for arr in resultX)
+    if not in_list:
+        resultX.append(i)
+        resultY.append(Y_set[iter])
+    iter = iter + 1
+X_set = np.array(resultX)
+Y_set = np.array(resultY)
+df = pd.DataFrame(X_set, Y_set)
+df.to_excel('Data.xlsx', index=False, header=False)
+
